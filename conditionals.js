934f7cc262e38a779f761403164f81a809b6995d
Kevin A. Bica <kalex.bica@gmail.com>
2019-12-17 17:11:50 +0000

    Use conditionals to tell people about the weather

---
+++ conditionals.js
@@ -0,0 +1,18 @@
+/*
+Required temperature for scarf prompt = any value less than zero (cold)
+Required temperature for short sleeves prompt = any value greater than 15 (warmish)
+*/
+
+const temperature = -200;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
+
+
